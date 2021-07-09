bf3b6c14b87740886fb3418e228d725a010e9cdc
GameBoyYeet <ericshurocks@gmail.com>
2021-07-08 17:55:55 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace lollmaorofl
+{
+    class Program
+    {
+    static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            while (true)
+            {
+                int num = rnd.Next(1, 4);
+                if (num == 1)
+                {
+                    Console.WriteLine("lmao");
+                }
+                else if (num == 2)
+                {
+                    Console.WriteLine("rofl");
+                }
+                else if (num == 3)
+                {
+                    Console.WriteLine("lol");
+                }
+                System.Threading.Thread.Sleep(50);
+            }
+        }
+
+        
+    }
+}
